1dbbf60beb6c18eee87d3f96b627a474080161fd
VynsD <v.dellavv@gmail.com>
2020-06-23 15:36:56 +0200

    ADD added footer and header

---
+++ bootshop/shared/components/footer.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function Footer() {
+  return (  
+    <div>
+      <p>
+        <span>data 1</span>
+      </p>
+      <p>
+        <span>data 2</span>
+      </p>
+      <p>
+        <span>data 3</span>
+      </p>
+      <p>
+        <span>data 4</span>
+      </p>
+    </div>
+  );
+}
+
+export default Footer;
