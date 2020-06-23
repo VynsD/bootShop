1ff12b603e3f8b620e968b0cb5426b262e627b2e
VynsD <v.dellavv@gmail.com>
2020-06-23 15:31:39 +0200

    ADD error and loading component

---
+++ bootshop/shared/components/error.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function ErrorFetchData() {
+  return (  
+    <div>
+      <h1>An Error Occurred</h1>
+      <p>
+        <span>Error During fetching data!</span>
+      </p>
+    </div>
+  );
+}
+
+export default ErrorFetchData;
