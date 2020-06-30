e598282b55a63d1192a19b5d38e4ccf0dbb600e0
VynsD <v.dellavv@gmail.com>
2020-06-30 14:49:58 +0200

    REDO refact card, error, loading, header

--- bootshop/shared/components/header.js
+++ bootshop/shared/components/header.js
@@ -4,7 +4,7 @@ function Header() {
   return (  
     <div className="header-wrapper">
       <div className="header_flex-center header_filler">{/*Menu*/}</div>
-      <h1 className="header_flex-center">Bootshop</h1>
+      <h1 className="header header_flex-center">Bootshop</h1>
       <div className="header_flex-center header_filler">{/*Search*/}</div>
     </div>
   );
--- bootshop/shared/components/loading.js
+++ bootshop/shared/components/loading.js
@@ -3,10 +3,10 @@ import React from 'react';
 function LoadingData() {
   return (  
     <div className="grid-onlyElement">
-      <div>
+      <div className="loading_wrapper">
         <h2 className="loading_text">Loading Data</h2>
+        <div className="loading_spinner"></div>
       </div>
-      <div className="loading_spinner"></div>
     </div>
   );
 }
